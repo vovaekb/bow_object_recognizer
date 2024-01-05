819c2957cc69f925146f24ad9f530a239025a4ea
Vladimir Privalov <vovaprivalov@gmail.com>
2024-01-05 14:49:40 +0500

    Refactor global variables with constexpr

--- bow_object_recognizer/include/bow_object_recognizer/common.h
+++ bow_object_recognizer/include/bow_object_recognizer/common.h
@@ -5,40 +5,40 @@
 #include "keypoint_detector.h"
 #include "feature_estimator.h"
 
-extern std::string training_dataset;
+constexpr std::string training_dataset;
 
-extern int vocabulary_size;
-extern int norm_est_k;
-extern float norm_rad;
-extern float descr_rad;
-extern float voxel_grid_size;
-extern float score_thresh;
-extern int best_models_k;
-extern bool perform_voxelizing;
+constexpr int vocabulary_size;
+constexpr int norm_est_k;
+constexpr float norm_rad;
+constexpr float descr_rad;
+constexpr float voxel_grid_size;
+constexpr float score_thresh;
+constexpr int best_models_k;
+constexpr bool perform_voxelizing;
 // Uniform sampling
-extern float us_radius;
+constexpr float us_radius;
 // ISS
-extern int iss_salient_rad_factor;
-extern int iss_non_max_rad_factor;
+constexpr int iss_salient_rad_factor;
+constexpr int iss_non_max_rad_factor;
 // Harris
-extern float harris_thresh;
-extern float harris_radius;
+constexpr float harris_thresh;
+constexpr float harris_radius;
 // General methods
-extern std::string keypoint_detector;
-extern std::string feature_descriptor;
-extern std::string test_scenes_dir;
-extern std::string test_scene;
-extern std::string scene_name;
-extern std::string gt_files_dir;
-extern std::string gt_file_path;
-extern std::string scene_descr_file_path;
-extern bool use_partial_views;
-extern bool apply_thresh;
-extern bool apply_verification;
-extern bool limit_matches;
-extern BoWModelDescriptorExtractor::Ptr bow_extractor;
-extern Source::Ptr training_source;
-extern KeypointDetector::Ptr detector;
-extern FeatureEstimator<PointType>::Ptr feature_estimator;
-extern std::map<std::string, BoWDescriptor> training_bow_descriptors;
+constexpr std::string keypoint_detector;
+constexpr std::string feature_descriptor;
+constexpr std::string test_scenes_dir;
+constexpr std::string test_scene;
+constexpr std::string scene_name;
+constexpr std::string gt_files_dir;
+constexpr std::string gt_file_path;
+constexpr std::string scene_descr_file_path;
+constexpr bool use_partial_views;
+constexpr bool apply_thresh;
+constexpr bool apply_verification;
+constexpr bool limit_matches;
+constexpr BoWModelDescriptorExtractor::Ptr bow_extractor;
+constexpr Source::Ptr training_source;
+constexpr KeypointDetector::Ptr detector;
+constexpr FeatureEstimator<PointType>::Ptr feature_estimator;
+constexpr std::map<std::string, BoWDescriptor> training_bow_descriptors;
 #endif // COMMON_H
